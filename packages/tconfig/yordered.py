c390c6a8de504ef487dc87da302c288f3d17f453
Henrique Moreira <h@serrasqueiro.com>
2020-01-26 18:42:38 +0000

    Basic dictionary order!

---
+++ packages/tconfig/yordered.py
@@ -0,0 +1,37 @@
+"""
+Yet another ordered directionary!
+
+(c)2020  Henrique Moreira (part of 'tconfig')
+"""
+
+
+#
+# CLASS DGeneric
+#
+class DGeneric():
+    def init_dgeneric (self, desc):
+        assert type( desc )==str
+        self.desc = desc
+
+
+#
+# CLASS DOrder
+#
+class DOrder():
+    def __init__ (self, data=None, desc=""):
+        self.init_dgeneric( desc )
+        self.data, self.keying = self._set_from_dict( data )
+
+
+    def _set_from_dict (self, data):
+        if data is None: return dict(), []
+        keys = list( data.keys() )
+        keys.order()
+        return data, keys
+
+#
+# Main script
+#
+if __name__ == "__main__":
+    print("""Import this module.
+""")
