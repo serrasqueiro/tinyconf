17cfda247d294c10c6f80b7a02134c3b5b0737b6
Henrique Moreira <h@serrasqueiro.com>
2020-05-03 12:52:48 +0100

    c_mul() for 32bit hashes

--- packages/xywinter/lehash.py
+++ packages/xywinter/lehash.py
@@ -9,6 +9,8 @@ def calc_p_hash (a_str, str_len=-1, a_mod=-1):
     """ Calculate basic hash """
     if a_mod == -1:
         quoc = 4294967295	# MAX_UINT32
+    elif a_mod == 0:
+        quoc = largest_prime()
     else:
         assert a_mod > 1
         quoc = a_mod
@@ -26,14 +28,26 @@ def calc_p_hash (a_str, str_len=-1, a_mod=-1):
     x = val << 7
     while True:
         idx += 1
-        x = (1000003 * x) ^ val
+        # c_mul() is similar to: x = (1000003 * x) ^ val
+        # But its scope is limited to uint32!
+        x = c_mul(1000003, x) ^ val
         if idx >= a_len:
             break
         val = ord(a_str[idx])
     z = x ^ a_len
-    if z == -1:
-        return -2
     return z % quoc
+
+#	class string:
+#	    def __hash__(self):
+#	        if not self:
+#	            return 0 # empty
+#	        value = ord(self[0]) << 7
+#	        for char in self:
+#	            value = c_mul(1000003, value) ^ ord(char)
+#	        value = value ^ len(self)
+#	        if value == -1:  /* removed in p_hash */
+#	            value = -2
+#	        return value
 
 # Equivalent C/ C++ function:
 #	long calc_p_hash (const char* str, int iLength)  /* originally cysw_str_hash() */
@@ -51,6 +65,12 @@ def calc_p_hash (a_str, str_len=-1, a_mod=-1):
 #	 if ( x==-1 ) return -2;
 #	 return x;
 #	}
+
+
+def c_mul(a, b):
+    """ 'cyclic' multiplication (without overflow) as in C
+    """
+    return (a * b) & 0xFFFFFFFF
 
 
 def largest_prime ():
