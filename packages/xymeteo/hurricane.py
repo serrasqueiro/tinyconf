6f178cfafc6657a34a485828c0d735196bc1e077
Henrique Moreira <h@serrasqueiro.com>
2020-10-05 18:37:23 +0100

    Added retired hurricane names, from NHC -- https://www.nhc.noaa.gov/aboutnames_history.shtml

--- packages/xymeteo/hurricane.py
+++ packages/xymeteo/hurricane.py
@@ -18,6 +18,8 @@ import sys
 
 _SAMPLES = (2020, 2021, 2022, 2023, 2024, 2025,
             )
+
+### Atlantic hurricane names!
 _NAMES_2020 = (
     "Arthur",
     "Bertha",
@@ -194,6 +196,104 @@ _SEQ_NAMES = ((2020,
                ),
               )
 
+_GREEK_STORMS = ("Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta",
+                 "Iota", "Kappa", "Lambda", "Mu", "Nu", "Xi", "Omicron", "Pi",
+                 "Rho", "Sigma", "Tau", "Upsilon", "Phi", "Chi", "Psi", "Omega",
+                 )
+# retired names: https://www.nhc.noaa.gov/aboutnames_history.shtml
+_RETIRED_HURRICANES = (
+    (1972, "Agnes"),
+    (1983, "Alicia"),
+    (1980, "Allen"),
+    (2001, "Allison"),
+    (1992, "Andrew"),
+    (1977, "Anita"),
+    (1957, "Audrey"),
+    (1965, "Betsy"),
+    (1967, "Beulah"),
+    (1991, "Bob"),
+    (1969, "Camille"),
+    (1961, "Carla"),
+    (1974, "Carmen"),
+    (1954, "Carol"),
+    (1970, "Celia"),
+    (1996, "Cesar"),
+    (2004, "Charley"),
+    (1964, "Cleo"),
+    (1955, "Connie"),
+    (1979, "David"),
+    (2007, "Dean"),
+    (2005, "Dennis"),
+    (1990, "Diana"),
+    (1955, "Diane"),
+    (1960, "Donna"),
+    (1964, "Dora"),
+    (1954, "Edna"),
+    (1985, "Elena"),
+    (1975, "Eloise"),
+    (2015, "Erika"),
+    (2003, "Fabian"),
+    (2007, "Felix"),
+    (1974, "Fifi"),
+    (1963, "Flora"),
+    (2018, "Florence"),
+    (1999, "Floyd"),
+    (1996, "Fran"),
+    (2004, "Frances"),
+    (1979, "Frederic"),
+    (1998, "Georges"),
+    (1988, "Gilbert"),
+    (1985, "Gloria"),
+    (1978, "Greta"),
+    (2008, "Gustav"),
+    (2017, "Harvey"),
+    (1961, "Hattie"),
+    (1954, "Hazel"),
+    (1964, "Hilda"),
+    (1996, "Hortense"),
+    (1989, "Hugo"),
+    (2010, "Igor"),
+    (2008, "Ike"),
+    (1966, "Inez"),
+    (2013, "Ingrid"),
+    (1955, "Ione"),
+    (2011, "Irene"),
+    (2001, "Iris"),
+    (2017, "Irma"),
+    (2003, "Isabel"),
+    (2002, "Isidore"),
+    (2004, "Ivan"),
+    (1955, "Janet"),
+    (2004, "Jeanne"),
+    (1988, "Joan"),
+    (2015, "Joaquin"),
+    (2003, "Juan"),
+    (2005, "Katrina"),
+    (2000, "Keith"),
+    (1990, "Klaus"),
+    (1999, "Lenny"),
+    (2002, "Lili"),
+    (1995, "Luis"),
+    (2017, "Maria"),
+    (1995, "Marilyn"),
+    (2016, "Matthew"),
+    (2018, "Michael"),
+    (2001, "Michelle"),
+    (1998, "Mitch"),
+    (2017, "Nate"),
+    (2007, "Noel"),
+    (1995, "Opal"),
+    (2016, "Otto"),
+    (2008, "Paloma"),
+    (2005, "Rita"),
+    (1995, "Roxanne"),
+    (2012, "Sandy"),
+    (2005, "Stan"),
+    (2010, "Tomas"),
+    (2005, "Wilma"),
+    )
+
+
 def main():
     """ Basic simple dump of db.
     """
@@ -218,6 +318,11 @@ def run_main(out, err, args) -> int:
         param = args[1:]
     if len(letter) != 1:
         return None
+    if letter == 'b':
+        assert not param
+        return run_b()
+    if letter == 'c':
+        return run_c(param)
     assert not param
     code = run_a()
     return code
@@ -239,6 +344,50 @@ def run_a() -> int:
         idx += 1
     assert idx == 6
     return 0
+
+
+def run_b() -> int:
+    """ Get storms from alphabetical greek letter """
+    letters = get_greek_storms()
+    idx = 0
+    for letter in letters:
+        latin = chr(ord('A')+idx)
+        print(latin, letter)
+        idx += 1
+    return 0
+
+
+def run_c(param) -> int:
+    """ Get list from year """
+    value = param[0]
+    rest = param[1:]
+    assert not rest
+    year = int(value)
+    print(f"get_hurricane_names({year}):")
+    names = get_hurricane_names(year)
+    print(f"names (#{len(names)}: {names}")
+    is_ok = 21 <= len(names) <= 26
+    assert is_ok
+    return 0
+
+
+def get_hurricane_names(year) -> tuple:
+    """ Returns the tuple of hurricanes for known year (>=2020)
+    """
+    invalid = ("?",)
+    if year < _SAMPLES[0]:
+        return invalid
+    ymod = (year-2020) % 6
+    names = list(_SEQ_NAMES[ymod][1][1])
+    return names
+
+
+def get_greek_storms() -> tuple:
+    """ Returns the list (as tuple) of the storms (greek letters).
+    """
+    tup = _GREEK_STORMS
+    assert len(tup) >= 20
+    return tup
 
 
 # Main...
