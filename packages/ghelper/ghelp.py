ae3da558ab350b2562d21b88f60a4d158cd83cae
Henrique Moreira <h@serrasqueiro.com>
2021-05-15 12:45:40 +0100

    First commit does not have a difference!

--- packages/ghelper/ghelp.py
+++ packages/ghelper/ghelp.py
@@ -5,28 +5,32 @@
 git helper functions
 """
 
-# pylint: disable=invalid-name
+# pylint: disable=unused-argument
 
 import os
 import ghelper.pgit as pgit
 
 
-def run_list(out_file, err_file, rp, param, debug=0):
+def run_list(out_file, err_file, rpl, param, debug=0):
     """ List function. """
-    assert param == []
-    _, refs = rp.pretty_log()
+    assert not param, "'param' expected empty!"
+    _, refs = rpl.pretty_log()
     bogus = list()
     if debug > 0:
         for ish, _, stamp in refs:
-            print(ish, stamp)
-    g = rp.git
-    dct = dict()
+            print("Debug: ish, stamp =", ish, stamp)
+    last_datelist = list()
+    dct, names = dict(), dict()
+    for fname in rpl.git.execute(["git", "ls-files"]).splitlines():
+        assert fname, "Unexpected empty file name"
+        names[fname] = ""
     for ish, adate, stamp in refs:
-        s = g.execute(["git", "diff-tree", "--no-commit-id", "--name-only", "-r", ish])
+        s = rpl.git.execute(["git", "diff-tree", "--no-commit-id", "--name-only", "-r", ish])
         change_list = s.splitlines()
         if debug > 0:
             print("Debug:", stamp, ":::", ish, change_list)
-        if change_list == []:
+        if not change_list:
+            last_datelist.append(adate)
             continue
         for fname in change_list:
             if fname not in dct:
@@ -34,10 +38,18 @@ def run_list(out_file, err_file, rp, param, debug=0):
             else:
                 if adate > dct[fname]:
                     dct[fname] = adate
-    lst = list(dct.keys())
-    lst.sort()
+            names[fname] = adate
+    # The first commit: there is no diff-tree, so files are at that time!
+    if last_datelist:
+        adate = last_datelist[0]
+        for fname in names:
+            if fname in dct:
+                continue
+            if debug > 0:
+                print("Debug: first commit:", adate, fname)
+            dct[fname] = adate
     queue = []
-    for fname in lst:
+    for fname in sorted(dct):
         adate = dct[fname].replace(",", " ")
         s = "{} {}".format(adate, fname)
         if len(adate) != pgit.ISO_DATE_LEN:
@@ -45,24 +57,24 @@ def run_list(out_file, err_file, rp, param, debug=0):
         queue.append(s)
     queue.sort(reverse=False)
     if out_file is not None:
-        s = "\n".join(queue)
-        out_file.write("{}\n".format(s))
-    if bogus != []:
+        astr = "\n".join(queue)
+        out_file.write("{}\n".format(astr))
+    if bogus:
         err_file.write("The following dates are wrong: {} ...\n".format(bogus[0]))
         return 1, queue
     return 0, queue
 
 
-def run_touch(out_file, err_file, rp, queue, opts, debug=0):
+def run_touch(out_file, err_file, rpl, queue, opts, debug=0):
     """ Run touch! """
     dry_run = opts["dry-run"]
     wide = pgit.ISO_DATE_LEN
     my_path = pgit.get_realpath(pgit.working_dir())
-    if my_path != rp.working_dir:
+    if my_path != rpl.working_dir:
         if debug > 0:
             print("# {}\n".format(my_path))
-        print("cd {}".format(rp.working_dir))
-    pgit.set_working_dir(rp.working_dir)
+        print("cd {}".format(rpl.working_dir))
+    pgit.set_working_dir(rpl.working_dir)
     fails = 0
     for q in queue:
         assert len(q) > wide + 2
@@ -80,25 +92,25 @@ def run_touch(out_file, err_file, rp, queue, opts, debug=0):
     return 0 if fails == 0 else 2
 
 
-def run_detail(err_file, rp, param, debug=0):
+def run_detail(err_file, rpl, param, debug=0):
     """ Detail function. """
     show_all = param == []
-    m = rp.heads.master
+    m = rpl.heads.master
     name = m.name
     if name != "master":
         err_file.write("Master is '{}', not master!\n".format(name))
         return 4
-    hc = rp.head.commit
-    tree = hc.tree
+    hcommit = rpl.head.commit
+    tree = hcommit.tree
     tree_textual = ["type({}): {}".format(x.type, x.name) for x in tree]
     print("cwd:", pgit.working_dir())
     print("Repo: {} (bare? {}), at: {}"
-          "".format(rp.repo_name, rp.bare, rp.working_dir))
+          "".format(rpl.named_as(), rpl.bare, rpl.working_dir))
     for txt in tree_textual:
         is_tree = txt.startswith("type(tree)")
         shown = txt+"/" if is_tree else txt
         print(shown)
-    lines, refs = rp.pretty_log()
+    lines, refs = rpl.pretty_log()
     if show_all:
         print("\nLog:::\n\n{}".format("\n".join(lines)))
     else:
